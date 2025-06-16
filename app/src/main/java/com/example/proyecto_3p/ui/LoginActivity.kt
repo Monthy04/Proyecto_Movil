47118f6d267b3f959ca5b9f95b1d99734f56de14
Monthy04 <spartan-0404@outlook.com>
2025-06-16 10:41:00 -0600

    commit

--- app/src/main/java/com/example/proyecto_3p/ui/LoginActivity.kt
+++ app/src/main/java/com/example/proyecto_3p/ui/LoginActivity.kt
@@ -2,41 +2,93 @@ package com.example.proyecto_3p.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.example.proyecto_3p.Administrador
+import com.example.proyecto_3p.Cliente
+import com.example.proyecto_3p.JsonStorage
 import com.example.proyecto_3p.MainActivity
+import com.example.proyecto_3p.Producto
 import com.example.proyecto_3p.R
+import com.example.proyecto_3p.Usuario
 import com.example.proyecto_3p.UsuarioManager
 
-class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class LoginActivity : AppCompatActivity()
+{
+    private lateinit var usuario: EditText
+    private lateinit var password: EditText
+    private lateinit var btnIngresa: Button
+    private lateinit var tvRegistrarse: TextView
+
+    private lateinit var clientesRegistrados: List<Cliente>
+    private lateinit var adminsRegistrados: List<Administrador>
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val etUsuario = findViewById<EditText>(R.id.editUsuario)
-        val etPassword = findViewById<EditText>(R.id.editPassword)
-        val btnIngresar = findViewById<Button>(R.id.btnIngresar)
-
-        btnIngresar.setOnClickListener {
-            val nombre = etUsuario.text.toString()
-            val pass = etPassword.text.toString()
-
-            val usuario = UsuarioManager.iniciarSesion(nombre, pass)
-
-            if (usuario != null) {
-                Toast.makeText(this, "Bienvenido ${usuario.nombre}", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                finish() // Cierra LoginActivity para que no se pueda regresar con el botón atrás
-            } else {
-                Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show()
-            }
-        }
-        val tvRegistrarse = findViewById<TextView>(R.id.tvRegistrate)
+        clientesRegistrados = JsonStorage.loadData(this, "clientesRegistrados.json") ?: emptyList()
+        adminsRegistrados = JsonStorage.loadData(this, "adminsRegistrados.json") ?: emptyList()
+        usuario = findViewById(R.id.editUsuario)
+        password = findViewById(R.id.editPassword)
+        btnIngresa = findViewById(R.id.btnIngresar)
+        tvRegistrarse = findViewById(R.id.tvRegistrate)
 
         tvRegistrarse.setOnClickListener {
             val intent = Intent(this, RegistroActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    fun onClick(view: View?)
+    {
+        when (view?.id)
+        {
+            R.id.btnIngresar -> ingresar();
+        }
+    }
+
+    private fun ingresar()
+    {
+        if(usuario.text.isNotEmpty() && usuario.text.isNotBlank() &&
+            password.text.isNotEmpty() && password.text.isNotBlank())
+        {
+            val nombre = usuario.text.toString()
+            val pass = password.text.toString()
+            var registrado = false
+            for(cliente in clientesRegistrados)
+            {
+                if(cliente.nombre.equals(nombre) && cliente.password.equals(pass))
+                {
+                    registrado = true
+                }
+            }
+            for(admin in adminsRegistrados)
+            {
+                if(admin.nombre.equals(nombre) && admin.password.equals(pass))
+                {
+                    registrado = true
+                }
+            }
+
+            if(registrado)
+            {
+                Toast.makeText(this, "Bienvenido $nombre", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("user", nombre)
+                startActivity(intent)
+                finish() // Cierra LoginActivity para que no se pueda regresar con el botón atrás
+            }
+            else
+            {
+                Toast.makeText(this,"Usuario no registrado", Toast.LENGTH_SHORT).show()
+            }
+        }
+        else
+        {
+            Toast.makeText(this,"Llenar datos", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/example/proyecto_3p/ui/RegistroActivity.kt
+++ app/src/main/java/com/example/proyecto_3p/ui/RegistroActivity.kt
@@ -1,77 +1,153 @@
 package com.example.proyecto_3p.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.example.proyecto_3p.R
-import com.example.proyecto_3p.UsuarioManager
 import com.example.proyecto_3p.Administrador
-import com.example.proyecto_3p.RepVentas
-import com.example.proyecto_3p.Usuario
+import com.example.proyecto_3p.Cliente
+import com.example.proyecto_3p.JsonStorage
 
-class RegistroActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class RegistroActivity : AppCompatActivity()
+{
+    private lateinit var nombre: EditText
+    private lateinit var password: EditText
+    private lateinit var correo: EditText
+    private lateinit var telefono: EditText
+    private lateinit var btnUnirse: Button
+    private lateinit var admin: CheckBox
+    private lateinit var tipoAdmin:RadioGroup
+    private lateinit var ventas: RadioButton
+    private lateinit var administ: RadioButton
+
+    private lateinit var clientesRegistrados: List<Cliente>
+    private val nuevosClientes = mutableListOf<Cliente>()
+    private lateinit var adminsRegistrados: List<Administrador>
+    private val nuevosAdmins = mutableListOf<Administrador>()
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
 
-        val etNombre = findViewById<EditText>(R.id.etNombre)
-        val etPassword = findViewById<EditText>(R.id.etRegPassword)
-        val etCorreo = findViewById<EditText>(R.id.etEmail)
-        val etTelefono = findViewById<EditText>(R.id.etTelefono)
-        val btnUnirse = findViewById<Button>(R.id.btnUnirse)
-        val cbAdmin = findViewById<CheckBox>(R.id.cbAdmin)
-        val rgTipoAdmin = findViewById<RadioGroup>(R.id.rgTipoAdmin)
-        val rbVentas = findViewById<RadioButton>(R.id.rbVentas)
-        val rbAdministracion = findViewById<RadioButton>(R.id.rbAdministracion)
+        nombre = findViewById(R.id.etNombre)
+        password = findViewById(R.id.etRegPassword)
+        correo = findViewById(R.id.etEmail)
+        telefono = findViewById(R.id.etTelefono)
+        btnUnirse = findViewById(R.id.btnUnirse)
+        admin = findViewById(R.id.cbAdmin)
+        tipoAdmin = findViewById(R.id.rgTipoAdmin)
+        ventas = findViewById(R.id.rbVentas)
+        administ = findViewById(R.id.rbAdministracion)
 
-        cbAdmin.setOnCheckedChangeListener { _, isChecked ->
-            rgTipoAdmin.visibility = if (isChecked) View.VISIBLE else View.GONE
+        clientesRegistrados = JsonStorage.loadData(this, "clientesRegistrados.json") ?: emptyList()
+        adminsRegistrados = JsonStorage.loadData(this, "adminsRegistrados.json") ?: emptyList()
+
+        for(cliente in clientesRegistrados)
+        {
+            nuevosClientes.add(cliente)
+        }
+        for(admin in adminsRegistrados)
+        {
+            nuevosAdmins.add(admin)
         }
 
-        btnUnirse.setOnClickListener {
-            val nombre = etNombre.text.toString()
-            val pass = etPassword.text.toString()
-            val correo = etCorreo.text.toString()
-            val tel = etTelefono.text.toString()
+        admin.setOnCheckedChangeListener { _, isChecked ->
+            tipoAdmin.visibility = if (isChecked) View.VISIBLE else View.GONE
+        }
+    }
 
-            if (nombre.isNotEmpty() && pass.isNotEmpty() && correo.isNotEmpty() && tel.isNotEmpty()) {
-                val usuario: Usuario = if (cbAdmin.isChecked) {
-                    if (rbAdministracion.isChecked) {
-                        Administrador().apply {
-                            tipo = 1
-                        }
-                    } else {
-                        RepVentas().apply {
-                            tipo = 0
-                        }
+    fun onClick(view: View?)
+    {
+        when (view?.id)
+        {
+            R.id.btnUnirse -> registrar()
+        }
+    }
+
+    private fun registrar()
+    {
+        val name = nombre.text
+        val pass = password.text
+        val email = correo.text
+        val tel = telefono.text
+
+        var usuarioRegistrado = false
+        if (name.isNotEmpty() && name.isNotBlank() &&
+            pass.isNotEmpty() && pass.isNotBlank() &&
+            email.isNotEmpty() && email.isNotBlank() &&
+            tel.isNotEmpty() && tel.isNotBlank())
+        {
+            if(admin.isChecked)
+            {
+                for(admin in adminsRegistrados)
+                {
+                    if(admin.nombre.equals(name.toString()))
+                    {
+                        usuarioRegistrado = true
                     }
-                } else {
-                    Usuario()
+                }
+                if(!usuarioRegistrado)
+                {
+                    val nuevoAdmin = Administrador()
+                    nuevoAdmin.nombre = name.toString()
+                    nuevoAdmin.password = pass.toString()
+                    nuevoAdmin.correo = email.toString()
+                    nuevoAdmin.telefono = tel.toString()
+                    nuevoAdmin.admin = admin.isChecked
+                    if(administ.isChecked)
+                    {
+                        nuevoAdmin.tipo = 1
+                    }
+                    else
+                    {
+                        nuevoAdmin.tipo = 0
+                    }
+                    nuevosAdmins.add(nuevoAdmin)
+                    JsonStorage.saveData(this,"adminsRegistrados.json",nuevosAdmins)
                 }
 
-                usuario.registro = UsuarioManager.usuariosRegistrados.size + 1
-                usuario.nombre = nombre
-                usuario.password = pass
-                usuario.correo = correo
-                usuario.telefono = tel
-                usuario.admin = cbAdmin.isChecked
-
-                // Verifica si ya existe
-                val existente = UsuarioManager.usuariosRegistrados.any {
-                    it.nombre == usuario.nombre
-                }
-
-                if (!existente) {
-                    UsuarioManager.usuariosRegistrados.add(usuario)
-                    Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show()
-                    finish()
-                } else {
-                    Toast.makeText(this, "El usuario ya existe", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                Toast.makeText(this, "Completa todos los campos", Toast.LENGTH_SHORT).show()
             }
+            else
+            {
+                for(cliente in clientesRegistrados)
+                {
+                    if(cliente.nombre.equals(name.toString()))
+                    {
+                        usuarioRegistrado = true
+                    }
+                }
+                if(!usuarioRegistrado)
+                {
+                    val nuevoCliente = Cliente()
+                    nuevoCliente.nombre = name.toString()
+                    nuevoCliente.password = pass.toString()
+                    nuevoCliente.correo = email.toString()
+                    nuevoCliente.telefono = tel.toString()
+                    nuevoCliente.admin = admin.isChecked
+                    nuevoCliente.compras = emptyList()
+                    nuevosClientes.add(nuevoCliente)
+                    JsonStorage.saveData(this,"clientesRegistrados.json",nuevosClientes)
+                }
+            }
+            if(usuarioRegistrado)
+            {
+                Toast.makeText(this, "Nombre de usuario no disponible", Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                Toast.makeText(this, "Usuario registrado", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        }
+        else
+        {
+            Toast.makeText(this, "Completa todos los campos", Toast.LENGTH_SHORT).show()
         }
     }
 }
